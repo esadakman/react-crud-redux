c48e2c234b8545a313d244a071731d42e3b7d064
esadakman <esadd26@gmail.com>
2022-09-25 21:14:27 +0300

    router structure, redux

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App'; 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-import {Provider} from 'react-redux';
-import {BrowseRouter} from 'react-router-dom';
-import store from './redux/store';
+import { Provider } from "react-redux"; 
+import store from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render( 
-
-  <BrowseRouter>
-  <Provider store={store}>
-
-    <App /> 
-  </Provider>
-  </BrowseRouter>
-);  
+    <Provider store={store}>
+      <App />
+    </Provider> 
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,93 @@
+import React, { useEffect } from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadUsers, UserDelete } from '../redux/action';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import { useNavigate } from 'react-router-dom';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
+
+
+const Home = () => {
+    let dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const data = useSelector((state) => state.data.users)
+    // console.log("data", data);
+    useEffect(() => {
+        dispatch(loadUsers());
+    }, []);
+
+    // const handleDelete = (id) => {
+    //     dispatch(UserDelete(id))
+    // }
+    return (
+        <div>
+            <div style={{marginTop:60,textAlign:'end',marginRight:35}}>
+                <Button variant="contained" onClick={() => navigate('addUser')}>User Add</Button>
+            </div>
+            <TableContainer component={Paper}>
+                <Table style={{ marginTop: 30 }} sx={{ minWidth: 900 }} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>Index</StyledTableCell>
+                            <StyledTableCell align="center">Name</StyledTableCell>
+                            <StyledTableCell align="center">Email</StyledTableCell>
+                            <StyledTableCell align="center">Contact</StyledTableCell>
+                            <StyledTableCell align="center">Address</StyledTableCell>
+                            <StyledTableCell align="center">Action</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data && data.map((user, index) => (
+                            <StyledTableRow key={user.id}>
+                                <StyledTableCell component="th" scope="row">
+                                    {index + 1}
+                                </StyledTableCell>
+                                <StyledTableCell align="center">{user.name}</StyledTableCell>
+                                <StyledTableCell align="center">{user.email}</StyledTableCell>
+                                <StyledTableCell align="center">{user.contact}</StyledTableCell>
+                                <StyledTableCell align="center">{user.address}</StyledTableCell>
+                                <StyledTableCell align="center">
+                                    <ButtonGroup variant="contained" aria-label="contained button">
+                                        {/* <Button style={{ marginRight: '5px' }} color="error" onClick={() => handleDelete(user.id)}>Delete</Button> */}
+                                        {/* <Button onClick={() => navigate(`editUser/${user.id}`)}>Edit</Button> */}
+                                        <Button></Button>
+                                    </ButtonGroup>
+                                </StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    )
+}
+
+export default Home
